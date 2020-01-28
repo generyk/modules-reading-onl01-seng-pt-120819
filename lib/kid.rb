d54ffd75ccc9e064d99ca714912a2a57ba5d53fe
Eryk Rachwal <erykrachwal@yahoo.com>
2020-01-28 19:19:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
-class Kid 
-  extend MetaDancing
+class Kid
+    extend FancyDance::ClassMethods 
+    include FancyDance::InstanceMethods
 
-end 
+    attr_accessor :name
+    def initialize(name)
+        @name = name 
+    end 
+end
